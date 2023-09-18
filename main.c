c6411d1b27b1579a0b5a8d3712a4057b6543009e
JuanR4140 <N/A>
2023-09-18 18:05:16 +0000

    Added warning to reading input approach in main.c

--- main.c
+++ main.c
@@ -15,6 +15,18 @@ int main(void) {
   initializeMemory(&memory);
   cpu.ax = 0;
   for(;;){
+
+    /*
+      This approach might not actually work! If a user
+      presses a key and 0x1 gets written, the CPU might
+      be executing some other instruction (JMP, CMP) that
+      won't give the program enough time to CMP the value
+      to get a new key, as the next cycle will immediately
+      set it back to 0! 
+
+      I propose 0x3CB stays on 0x1 and it's the programmer's
+      job to write 0x0 back to it to "acknowledge" the read key.
+    */
     memory.data[0x3CB] = 0x0;
     int ch = getchar();
     if(ch != ERR){
